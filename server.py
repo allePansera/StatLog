7839a8d342379c3089a2f8b7906245f1e99e8b7a
allePansera <allepansera2001@gmail.com>
2023-04-15 16:24:45 +0200

    Server continuous deployment

--- server.py
+++ server.py
@@ -5,3 +5,20 @@ from view.CalcView import CalcView
 app = Flask(__name__)
 PageView.register(app)
 CalcView.register(app)
+
+@app.route("/clone_master",methods=["POST"])
+def clone_master():
+    """Il metodo crea un endpoint da fornire a git hub affinchè possa gestire in autonomia le pull in arrivo sul branch
+    di nostro interesse. I dati di configurazione sono gestiti all'interno del file Config/Git/PAT.json"""
+    try:
+        # salvo il nome delle variabili che servono per scaricare gli aggiornamenti
+        branch_name, repo_name = "master", "StatLog"
+        repo = git.Repo(f"./")
+        origin = repo.remotes.origin
+        repo.create_head(f'{branch_name}',
+                         origin.refs.Testing).set_tracking_branch(origin.refs.Testing).checkout()
+        origin.pull()
+        # conclusa la sincronizzazione con git hub ritorno un success
+        return f"Sincronizzazione conclusa con successo", 200
+    except Exception as e:
+        return f"Errore in fase di sincronizzazione con Git:<br>{e}", 404
