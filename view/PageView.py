46e1c02f9028570b3d164a6799dbe87d3bb7519e
allePansera <allepansera2001@gmail.com>
2023-04-19 10:40:18 +0200

    Fontawesome support

--- view/PageView.py
+++ view/PageView.py
@@ -4,9 +4,6 @@ from flask import render_template, request
 
 class PageView(FlaskView):
 
-    def index(self):
-        return render_template("landing_page.html")
-
     @route('/stat_log')
     def stat_log(self):
         return render_template("stat_log.html")
